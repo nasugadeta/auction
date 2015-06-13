0cf07fd4ee4f7cd86bef2451ec24de0964d522d8
松永寛史 <hiromatsu0707@gmail.com>
2015-06-13 18:07:39 +0900

    新規登録機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,31 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    #formからデータを受けとる
+    @item = Item.new(item_params)
+    #これを保存する
+    @item.save
+    #show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  def item_params
+    #params.require(:key).permit(:filter)
+    params.require(:item).permit(
+      :name,
+      :price,
+      :seller_id,
+      :description,
+      :email,
+      :image_url
+      )
+  end
 end
